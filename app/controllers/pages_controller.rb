ea2c4d54e1aaedf129bd8e73c58b0b08f070bfe0
Artem Zemskov <art.zemskov88@gmail.com>
2025-03-11 14:45:23 +0100

    added homepage controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
+    @stores = Store.all
+    @best_deals = Deal
+      .where("expiry_date >= ?", Date.today)
+      .order(discounted_price: :desc)
+      .limit(10)
+
+    if params[:query].present?
+      @search_results = Product.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @search_results = []
+    end
   end
 end
