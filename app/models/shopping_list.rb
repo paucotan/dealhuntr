c6872424068fa96c74862b052aec5ced454ff29f
paulcohentannugi <ctannugipaul@gmail.com>
2025-03-11 14:12:21 +0100

    added model validation, added expirydate to deal

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,4 +1,7 @@
 class ShoppingList < ApplicationRecord
   belongs_to :user
   belongs_to :deal
+
+  validates :user_id, presence: true
+  validates :deal_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_many :favourites
+  has_many :shopping_lists
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
