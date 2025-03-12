999e08dbdf9a448253aa7cafb3be5cb16a8f04f0
Artem Zemskov <art.zemskov88@gmail.com>
2025-03-12 14:07:16 +0100

    added search path and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,23 @@
 class PagesController < ApplicationController
   def home
-    if params[:query].present?
-      @products = Product.search_by_name_and_category(params[:query])
-    else
-      @products = Product.all
-    end
-    
     @stores = Store.all
-
     @deals = Deal
       .where("expiry_date >= ?", Date.today)
       .order(discounted_price: :asc)
       .limit(20)
 
-    @search_results = if params[:query].present?
-      Product.where("name ILIKE ?", "%#{params[:query]}%")
+    # @search_results = if params[:query].present?
+    #   Product.where("name ILIKE ?", "%#{params[:query]}%")
+    # else
+    #   nil
+    # end
+  end
+
+  def search
+    if params[:query].present?
+      @products = Product.search_by_name_and_category(params[:query])
     else
-      nil
+      @products = Product.none
     end
   end
 
