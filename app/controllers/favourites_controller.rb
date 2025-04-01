e0f573abbd3ac6aa0fd879e01b444879a6c5510c
paulcohentannugi <ctannugipaul@gmail.com>
2025-04-01 13:02:00 +0200

    fixed issue with deals cards favourite button not working properly

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -5,6 +5,8 @@ class FavouritesController < ApplicationController
     @product = Product.find(params[:product_id])
     @favourite = current_user.favourites.find_or_create_by(product: @product)
     authorize @favourite
+    # Find the deal associated with this product (you may need to adjust this logic)
+    @deal = Deal.find_by(product_id: @product.id) # Adjust based on your associations
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_back fallback_location: root_path }
@@ -12,16 +14,20 @@ class FavouritesController < ApplicationController
   end
 
   def destroy
-    @favourite = current_user.favourites.find(params[:id])
-    authorize @favourite
-    @product = @favourite.product
-    @favourite.destroy
+    @favourite = current_user.favourites.find_by(id: params[:id])
+    authorize @favourite if @favourite
+    if @favourite
+      @product = @favourite.product
+      @deal = Deal.find_by(product_id: @product.id) # Adjust based on your associations
+      @favourite.destroy
+    else
+      Rails.logger.warn("Favourite with id=#{params[:id]} not found for user=#{current_user.id}")
+    end
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_back fallback_location: root_path }
     end
   end
-
 
   def index
     @favourites = policy_scope(current_user.favourites.includes(:product))
